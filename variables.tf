04cdf2ce48d63ff360a6d2f30b2f346ffa379a6c
Rigel Reyes <rigel.reyes206@t-mobile.com>
2020-12-03 13:44:26 -0600

    Removed GCP from options

--- variables.tf
+++ variables.tf
@@ -39,19 +39,6 @@ variable "instance_password" {
   description = "Password to connect to instances"
 }
 
-# GCP Variables
-variable "google_credentials" {
-  description = "Google Cloud Credentials"
-}
-
-variable "google_region" {
-  description = "Google Cloud Region"
-}
-
-variable "google_project" {
-  description = "Google Cloud Project"
-}
-
 # Azure Variables
 variable "azure_subscription_id" {
   description = "Azure Subscription ID"
